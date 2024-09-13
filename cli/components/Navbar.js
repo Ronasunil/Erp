46d7dbd3b6c06397bc833acc55a193eebba377d8
Ronasunil <ronasunilcoc@gmail.com>
2024-09-13 19:29:50 +0530

    ui cleaning

--- cli/components/Navbar.js
+++ cli/components/Navbar.js
@@ -3,9 +3,9 @@ import { NavLink } from "./Navlink";
 
 export function Navbar() {
   return (
-    <nav className="bg-gray-800 p-4">
+    <nav className="bg-gray-800 p-4 text-white pt-10 pb-10">
       <div className="container mx-auto flex items-center justify-between">
-        <div className="text-white text-2xl font-bold">
+        <div className=" text-2xl font-bold">
           <Link href="/">ERP</Link>
         </div>
 
@@ -17,7 +17,7 @@ export function Navbar() {
         </div>
 
         <div>
-          <NavLink href="/login">Login</NavLink>
+          <p className="text-xl font-semibold">Rona</p>
         </div>
       </div>
     </nav>
